c4ae8cefeb4fcab0999075a61697ecd2cd6f5dc4
Aryan Chouksey <ayushchauksey08@gmail.com>
2024-06-12 17:42:04 +0530

    Camera Component Error : to be fixed

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { CameraView , CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library'
+import { useEffect, useRef, useState } from 'react';
 
 export default function App() {
+  const [hasCameraPermissions, setHasCameraPermissions] = useState(null);
+  const [image, setImage] = useState(null);
+  const [facing, setFacing] = useState('back');
+  const [flash, setFlash] = useState('off');
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await  CameraView.requestCameraPermissionsAsync();
+      setHasCameraPermissions(cameraStatus.status === 'granted');
+    })();
+  
+  }, [])
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
